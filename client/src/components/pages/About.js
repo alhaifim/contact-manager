e0b729bb84eb673c20f53e403b77be6ba1de1360
alhaifim <alhaifim@outlook.com>
2019-12-06 21:31:15 -0700

    Cleaned up React App, created Navbar and Routers

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+           <h1>About Contact Manger App</h1> 
+           <p className = "my-1">
+           This is a great app that gives makes your contacts avaiable everywhere
+           </p>
+           <p className="bg-dark p">
+           <strong>Version: </strong> 1.0.0
+           </p>
+        </div>
+    )
+}
+export default About;
